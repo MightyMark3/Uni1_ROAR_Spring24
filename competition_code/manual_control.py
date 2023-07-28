01e01216b6989b131c3932b8c178c2ad783cd63e
tlzh0001 <ztl1998@berkeley.edu>
2023-07-28 17:48:51 -0400

    adding manual control to use viewer interface; try to move example_controller solution to here

---
+++ competition_code/manual_control.py
@@ -0,0 +1,124 @@
+import roar_py_carla
+import roar_py_interface
+import carla
+import pygame
+from PIL.Image import Image
+import numpy as np
+import asyncio
+from typing import Optional, Dict, Any
+
+class ManualControlViewer:
+    def __init__(
+        self
+    ):
+        self.screen = None
+        self.clock = None
+        self.last_control = {
+            "throttle": 0.0,
+            "steer": 0.0,
+            "brake": 0.0,
+            "hand_brake": np.array([0]),
+            "reverse": np.array([0])
+        }
+    
+    def init_pygame(self, x, y) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((x, y), pygame.HWSURFACE | pygame.DOUBLEBUF)
+        pygame.display.set_caption("RoarPy Manual Control Viewer")
+        pygame.key.set_repeat()
+        self.clock = pygame.time.Clock()
+
+    def render(self, image : roar_py_interface.RoarPyCameraSensorData, occupancy_map : Optional[Image] = None) -> Optional[Dict[str, Any]]:
+        image_pil : Image = image.get_image()
+        occupancy_map_rgb = occupancy_map.convert("RGB") if occupancy_map is not None else None
+        if self.screen is None:
+            if occupancy_map_rgb is None:
+                self.init_pygame(image_pil.width, image_pil.height)
+            else:
+                self.init_pygame(image_pil.width + occupancy_map.width, image_pil.height)
+        
+        new_control = {
+            "throttle": 0.0,
+            "steer": 0.0,
+            "brake": 0.0,
+            "hand_brake": np.array([0]),
+            "reverse": np.array([0])
+        }
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return None
+        
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_UP]:
+            new_control['throttle'] = 0.4
+        if pressed_keys[pygame.K_DOWN]:
+            new_control['brake'] = 0.2
+        if pressed_keys[pygame.K_LEFT]:
+            new_control['steer'] = -0.2
+        if pressed_keys[pygame.K_RIGHT]:
+            new_control['steer'] = 0.2
+        
+        image_surface = pygame.image.fromstring(image_pil.tobytes(), image_pil.size, image_pil.mode).convert()
+        if occupancy_map_rgb is not None:
+            occupancy_map_surface = pygame.image.fromstring(occupancy_map_rgb.tobytes(), occupancy_map_rgb.size, occupancy_map_rgb.mode).convert()
+
+        self.screen.fill((0,0,0))
+        self.screen.blit(image_surface, (0, 0))
+        if occupancy_map_rgb is not None:
+            self.screen.blit(occupancy_map_surface, (image_pil.width, 0))
+
+        pygame.display.flip()
+        self.clock.tick(60)
+        self.last_control = new_control
+        return new_control
+
+
+async def main():
+    carla_client = carla.Client('localhost', 2000)
+    carla_client.set_timeout(5.0)
+    roar_py_instance = roar_py_carla.RoarPyCarlaInstance(carla_client)
+    
+    carla_world = roar_py_instance.world
+    carla_world.set_asynchronous(True)
+    carla_world.set_control_steps(0.0, 0.005)
+    
+    # spawn_point, spawn_rpy = carla_world.spawn_points[
+    #     np.random.randint(len(carla_world.spawn_points))
+    # ]
+
+    spawn_point, spawn_rpy =carla_world.spawn_points[1]
+    
+    print("Spawning vehicle at", spawn_point, spawn_rpy)
+
+    vehicle = carla_world.spawn_vehicle(
+        "vehicle.tesla.model3",
+        spawn_point + np.array([0, 0, 2.0]),
+        spawn_rpy
+    )
+
+    print(vehicle.bounding_box)
+
+    camera = vehicle.attach_camera_sensor(
+        roar_py_interface.RoarPyCameraSensorDataRGB, # Specify what kind of data you want to receive
+        np.array([-2.0 * vehicle.bounding_box.extent[0], 0.0, 3.0 * vehicle.bounding_box.extent[2]]), # relative position
+        np.array([0, 10/180.0*np.pi, 0]), # relative rotation
+    )
+
+    viewer = ManualControlViewer()
+
+    try:
+        while True:
+            # img.get_image().save("test.png")
+            await carla_world.step()
+            img : roar_py_interface.RoarPyCameraSensorDataRGB = await camera.receive_observation()
+            control = viewer.render(img)
+            if control is None:
+                break
+            await vehicle.apply_action(control)
+    finally:
+        roar_py_instance.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- competition_code/submission.py
+++ competition_code/submission.py
@@ -5,8 +5,21 @@ Please do not change anything else but fill out the to-do sections.
 
 from typing import List, Tuple, Dict, Optional
 import roar_py_interface
+import numpy as np
+from manual_control import ManualControlViewer
 
+def normalize_rad(rad : float):
+    return (rad + np.pi) % (2 * np.pi) - np.pi
 
+def filter_waypoints(location : np.ndarray, current_idx: int, waypoints : List[roar_py_interface.RoarPyWaypoint]) -> int:
+    def dist_to_waypoint(waypoint : roar_py_interface.RoarPyWaypoint):
+        return np.linalg.norm(
+            location[:2] - waypoint.location[:2]
+        )
+    for i in range(current_idx, len(waypoints) + current_idx):
+        if dist_to_waypoint(waypoints[i%len(waypoints)]) < 3:
+            return i % len(waypoints)
+    return current_idx
 
 class RoarCompetitionSolution:
     async def __init__(
@@ -30,7 +43,9 @@ class RoarCompetitionSolution:
         self.collision_sensor = collision_sensor
         # TODO: You can do some initial computation here if you want to.
         # For example, you can compute the path to the first waypoint.
+        self.manual_viewer = ManualControlViewer()
 
+        
     async def step(
         self
     ) -> None:
@@ -39,4 +54,48 @@ class RoarCompetitionSolution:
         You can do whatever you want here, including apply_action() to the vehicle.
         """
         # TODO: Implement your solution here.
-        pass
+        vehicle_location = self.vehicle.get_3d_location()
+        vehicle_rotation = self.vehicle.get_roll_pitch_yaw()
+
+        # Receive camera data and render it
+        camera_data = await self.camera.receive_observation()
+        render_ret = self.manual_viewer.render(camera_data)
+         # If user clicked the close button, render_ret will be None
+        if render_ret is None:
+            return
+        
+        way_points = self.maneuverable_waypoints
+        # Find the waypoint closest to the vehicle
+        current_waypoint_idx = filter_waypoints(
+            vehicle_location,
+            current_waypoint_idx,
+            way_points
+        )
+         # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
+        waypoint_to_follow = way_points[(current_waypoint_idx + 3) % len(way_points)]
+
+        # Calculate delta vector towards the target waypoint
+        vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
+        heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
+
+        # Calculate delta angle towards the target waypoint
+        delta_heading = normalize_rad(heading_to_waypoint - vehicle_rotation[2])
+
+        # Proportional controller to steer the vehicle towards the target waypoint
+        steer_control = (
+            -8.0 / np.sqrt(np.linalg.norm(self.vehicle.get_linear_3d_velocity())) * delta_heading / np.pi
+        ) if np.linalg.norm(self.vehicle.get_linear_3d_velocity()) > 1e-2 else -np.sign(delta_heading)
+        steer_control = np.clip(steer_control, -1.0, 1.0)
+
+        # Proportional controller to control the vehicle's speed towards 40 m/s
+        throttle_control = 0.05 * (20 - np.linalg.norm(self.vehicle.get_linear_3d_velocity()))
+
+        control = {
+            "throttle": np.clip(throttle_control, 0.0, 1.0),
+            "steer": steer_control,
+            "brake": np.clip(-throttle_control, 0.0, 1.0),
+            "hand_brake": 0.0,
+            "reverse": 0,
+            "target_gear": 0
+        }
+        await self.vehicle.apply_action(control)
